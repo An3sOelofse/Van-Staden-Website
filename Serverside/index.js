b7b1a1461861523a5e4367b35edeffad69c2ef35
Andries Oelofse <an3soelofse@gmail.com>
2020-07-28 17:41:26 +0200

    added data sending to server

--- Serverside/index.js
+++ Serverside/index.js
@@ -1,6 +1,16 @@
 console.log("app is running...");
 
 const express = require('express');
+const { response } = require('express');
 const app = express();
 app.listen(3000, () => console.log("server is running..."));
 app.use(express.static("public"));
+app.use(express.json());
+app.post("/sendData",receiveData);
+
+function receiveData(request,response){
+    console.log(request.body);
+    response.json({
+        status:'success'
+    });
+}
