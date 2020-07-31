7efe41a77aff7bc4221fb1790dcfd7834103fcab
Andries Oelofse <an3soelofse@gmail.com>
2020-07-31 15:27:30 +0200

    moved files

---
+++ Serverside/public/Store/store.js
@@ -0,0 +1,7 @@
+let containers = document.getElementsByClassName("cardContainer");
+let cardArray = [];
+for(let i = 0; i < 0; i++ ){
+     cardArray[i] = document.createElement("div");
+     cardArray[i].className = "cardsmall";
+     containers[0].appendChild(cardArray[i]);
+}
