b9a204a42f6379bb479fa315df231937ad7dacc4
Andries Oelofse <an3soelofse@gmail.com>
2020-08-13 13:41:52 +0200

    added email sending function

--- Serverside/index.js
+++ Serverside/index.js
@@ -1,16 +1,9 @@
-console.log("app is running...");
-
-const sendToEmail = 'an3soelofse@gmail.com';
-
 require('dotenv').config();
 const express = require('express');
 const fs = require('fs');
 const nodemailer = require('nodemailer');
 const port = process.env.PORT||3000;
 
-let transporter = nodemailer.createTransport({
-    
-});
 
 const { response, json } = require('express');
 const app = express();
@@ -21,45 +14,46 @@ app.use(express.json());
 
 let rawdata = fs.readFileSync('products.json');
 let products = JSON.parse(rawdata);
-
-/////////////////////////////////////
 sendJSON("/cardInfo",products);
-
 function sendJSON(path,data){
     app.get(path,(req,res) => {
         res.json(data);
     });
 }
-/////////////////////////////////////////
-app.post("/sendData",receiveData);
 
+app.post("/sendData",receiveData);
 function receiveData(request,response){
     console.log(request.body);
     response.json({
         status:'success'
     });
 }
-/////////////////////////////////////////
 
+//sendEmail('an3soelofse@gmail.com','#Hackerman');
 function sendEmail(address,messageString){
 
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'sniper.target.diy.online',
-            pass: 'an3soelofse'
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
         }
     });
 
     let mailoptions = {
-        from: 'sniper.target.diy.online',
+        from: process.env.EMAIL,
         to: address,
         subject: 'test',
-        message: messageString
+        text: messageString
     }
 
     transporter.sendMail(mailoptions,(err,inf) =>{
-        if(err)
+        if(err){
+            console.log('Email error: ',err);
+        }
+        else{
+            console.log('email sent... ',inf);
+        }
     });
 
 
